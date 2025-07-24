b93bca44d47953932a96a446e3de21208cd72c16
ilydatutunc <ilydatutunc@gmail.com>
2025-07-24 17:32:30 +0300

    Experiment with different C values in logistic regression

--- hyperparameter_experiments.py
+++ hyperparameter_experiments.py
@@ -26,29 +26,27 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-max_features = None
+max_features_values = [1000, 2000, 3000, None]
 ngram = (1,1)  #digerleri sabit tutulan kosularda en yuksek accuracy degerine sahip
-C_values = [0.01, 0.1, 1.0, 10.0, 100.0]
 
-for C in C_values:
-    
+for max_features in max_features_values:
     vectorizer = TfidfVectorizer(stop_words='english', ngram_range=ngram, max_features=max_features)
     X_train_vectorized = vectorizer.fit_transform(X_train)
     X_test_vectorized = vectorizer.transform(X_test)
 
+    model = LogisticRegression(C=100.0, max_iter=200)
+    model.fit(X_train_vectorized, y_train)
 
-    model = LogisticRegression(C=C, max_iter=200) #kullanacagim modeli olusturdum._
-    model.fit(X_train_vectorized, y_train) #train ile modeli egit (fit)
-
-
+    
     y_pred = model.predict(X_test_vectorized)
     acc = accuracy_score(y_test, y_pred)
 
+    
     with mlflow.start_run():
         mlflow.log_param("ngram_range", ngram)
-        mlflow.log_param("C", C)  
-        mlflow.log_param("max_features", None)  # default
+        mlflow.log_param("C", 100.0) #en yüksek accuracy degerini veren C degeri
+        mlflow.log_param("max_features", max_features)
         mlflow.log_metric("accuracy", acc)
         mlflow.sklearn.log_model(model, "model")
 
-print(f"C={C}, accuracy={acc:.4f}")
+    print(f"max_features={max_features}, accuracy={acc:.4f}")
